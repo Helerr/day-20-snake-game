f2c93fc159d63e67b2015fc1679560de58dced4a
Neculai Alexandru <alleexx250595@gmail.com>
2022-03-18 16:55:18 +0200

    - highscore - nonpersistent - snake reset - score reset

--- main.py
+++ main.py
@@ -35,15 +35,15 @@ while game_is_on:
         scoreboard.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        snake.reset()
+        scoreboard.reset()
 
     # Detect collision with tail
     # if head collides with any segment in the tail, trigger game_over()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            snake.reset()
+            scoreboard.reset()
 
 screen.exitonclick()
